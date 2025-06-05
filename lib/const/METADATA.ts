af7d4ef1c121c36819e9d0301ac767073c27ebe6
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2025-06-05 17:05:40 +0300

    add mui

---
+++ lib/const/METADATA.ts
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const METADATA: Metadata = {
+  title: "Berat BAYRAM Portfolio",
+  description:
+    "Berat BAYRAM's personal portfolio showcasing projects and skills.",
+  keywords: [
+    "Berat BAYRAM",
+    "portfolio",
+    "web developer",
+    "software engineer",
+    "projects",
+    "skills",
+    "Next.js",
+    "React",
+    "TypeScript",
+    "JavaScript",
+  ],
+};

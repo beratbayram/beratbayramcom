5b21745ca75ae6d8e8f148530d015ea1c88f180f
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2025-06-29 17:50:26 +0300

    disable links temporarily

--- lib/components/BrHeader.tsx
+++ lib/components/BrHeader.tsx
@@ -6,15 +6,19 @@ import Image from "next/image";
 import IconButton from "@mui/material/IconButton";
 import { BrLinks } from "./BrLinks";
 
+const ENABLE_LINKS = false;
+
 export function BrHeader() {
   return (
     <header className="sticky top-0 right-0 left-0 z-10 flex items-center justify-between gap-4 bg-white py-4 dark:bg-gray-900">
       <h1 className="justify-start-safe flex flex-1 items-center text-2xl font-bold">
         <Link href="/">Berat BAYRAM</Link>
       </h1>
-      <nav className="flex flex-1 justify-center-safe space-x-4 *:text-lg *:hover:underline">
-        <BrLinks />
-      </nav>
+      {ENABLE_LINKS && (
+        <nav className="flex flex-1 justify-center-safe space-x-4 *:text-lg *:hover:underline">
+          <BrLinks />
+        </nav>
+      )}
       <nav className="flex flex-1 justify-end-safe space-x-4">
         <Link href="https://linkedin.com/in/berat-bayram/">
           <IconButton aria-label="linkedin" color="inherit">

669be4f90877ec9ca1bdb724b63aa372b824b591
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2025-08-03 21:20:49 +0300

    fix: update disallow rules in robots.txt to remove /admin/ and /_next/

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/private/", "/admin/", "/_next/"],
+      disallow: ["/private/"],
     },
     sitemap: "https://beratbayram.com/sitemap.xml", // Update with your actual domain
   };
